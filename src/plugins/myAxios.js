3c0879c9f886abf0313fdc77ea848651448b2fe5
mojiez <937691466@qq.com>
2024-04-11 22:04:05 +0800

    全局请求拦截器实现强制登陆 路径#待解决

--- src/plugins/myAxios.js
+++ src/plugins/myAxios.js
@@ -11,6 +11,7 @@ myAxios.defaults.withCredentials = true;
 
 // 添加请求拦截器
 // Add a request interceptor
+// 这是一个全局的请求拦截器 每次给后端发送请求前执行
 myAxios.interceptors.request.use(function (config) {
     // Do something before request is sent
     console.log("请求发送了",config);
@@ -21,9 +22,17 @@ myAxios.interceptors.request.use(function (config) {
   });
 
 // Add a response interceptor
+// 全局相应拦截器 每次接受到后端返回后 先执行这个
 myAxios.interceptors.response.use(function (response) {
     // Do something with response data
     console.log("请求收到了",response);
+
+    // 如果收到的reponse.data.code 是40101 说明当前没有登陆
+    // 强制跳转到登录页面
+    if(response?.data?.code === 40101) {
+      // todo 为什么会有一个#号
+      window.location.href = '/#/login' 
+    }
     return response.data;
   }, function (error) {
     // Do something with response error
